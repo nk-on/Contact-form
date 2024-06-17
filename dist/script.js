eb214ef72f9e5ba04ce4079d31b25b0652277e05
nk-on <johndoe@example.com>
2024-06-17 22:47:58 +0400

    Choosing html input labels and error messeges

---
+++ dist/script.js
@@ -0,0 +1,5 @@
+"use strict";
+const firstNameInput = document.querySelector('#first-name');
+const firstNameErrorMessage = document.querySelector('#error-message-firstName');
+const lastNameInput = document.querySelector('#last-name');
+const lastNameErrorMessege = document.querySelector('#error-message-lastName');
---
+++ src/script.ts
@@ -0,0 +1,4 @@
+const firstNameInput = document.querySelector('#first-name') as HTMLInputElement;
+const firstNameErrorMessage = document.querySelector('#error-message-firstName') as HTMLLabelElement;
+const lastNameInput = document.querySelector('#last-name') as HTMLInputElement;
+const lastNameErrorMessege =  document.querySelector('#error-message-lastName') as HTMLLabelElement;
