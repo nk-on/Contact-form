445119d885edca060eee6a182f6f5898d9133dfc
nk-on <johndoe@example.com>
2024-06-18 21:14:33 +0400

    Feat:Implementing system which checks if user entered Name and Last name

--- dist/script.js
+++ dist/script.js
@@ -3,3 +3,10 @@ const firstNameInput = document.querySelector('#first-name');
 const firstNameErrorMessage = document.querySelector('#error-message-firstName');
 const lastNameInput = document.querySelector('#last-name');
 const lastNameErrorMessege = document.querySelector('#error-message-lastName');
+const submitButton = document.querySelector('#submit-button');
+function checkInput(e) {
+    e.preventDefault();
+    firstNameInput.value === '' ? firstNameErrorMessage.style.display = 'block' : firstNameErrorMessage.style.display = 'none';
+    lastNameInput.value === '' ? lastNameErrorMessege.style.display = 'block' : lastNameErrorMessege.style.display = 'none';
+}
+submitButton.addEventListener('click', checkInput);
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,11 @@
 const firstNameInput = document.querySelector('#first-name') as HTMLInputElement;
 const firstNameErrorMessage = document.querySelector('#error-message-firstName') as HTMLLabelElement;
 const lastNameInput = document.querySelector('#last-name') as HTMLInputElement;
-const lastNameErrorMessege =  document.querySelector('#error-message-lastName') as HTMLLabelElement;
+const lastNameErrorMessege = document.querySelector('#error-message-lastName') as HTMLLabelElement;
+const submitButton = document.querySelector('#submit-button') as HTMLButtonElement;
+function checkInput(e: any): void {
+    e.preventDefault();
+    firstNameInput.value === '' ?  firstNameErrorMessage.style.display = 'block' : firstNameErrorMessage.style.display = 'none';
+    lastNameInput.value === '' ? lastNameErrorMessege.style.display = 'block' : lastNameErrorMessege.style.display = 'none';
+}
+submitButton.addEventListener('click', checkInput)
