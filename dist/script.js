7daab305062ccb16d9c906c7ed96576bb011ff5b
nk-on <johndoe@example.com>
2024-06-18 21:41:31 +0400

    Feat:implementing email validation system

--- dist/script.js
+++ dist/script.js
@@ -4,8 +4,16 @@ const firstNameErrorMessage = document.querySelector('#error-message-firstName')
 const lastNameInput = document.querySelector('#last-name');
 const lastNameErrorMessege = document.querySelector('#error-message-lastName');
 const submitButton = document.querySelector('#submit-button');
+const emailInput = document.querySelector('#Email');
+const emailErrorMessege = document.querySelector('#error-message-email');
+function checkEmail() {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const validEmail = emailRegex.test(emailInput.value);
+    validEmail ? emailErrorMessege.style.display = 'none' : emailErrorMessege.style.display = 'block';
+}
 function checkInput(e) {
     e.preventDefault();
+    checkEmail();
     firstNameInput.value === '' ? firstNameErrorMessage.style.display = 'block' : firstNameErrorMessage.style.display = 'none';
     lastNameInput.value === '' ? lastNameErrorMessege.style.display = 'block' : lastNameErrorMessege.style.display = 'none';
 }
--- src/script.ts
+++ src/script.ts
@@ -3,9 +3,17 @@ const firstNameErrorMessage = document.querySelector('#error-message-firstName')
 const lastNameInput = document.querySelector('#last-name') as HTMLInputElement;
 const lastNameErrorMessege = document.querySelector('#error-message-lastName') as HTMLLabelElement;
 const submitButton = document.querySelector('#submit-button') as HTMLButtonElement;
+const emailInput = document.querySelector('#Email') as HTMLInputElement;
+const emailErrorMessege = document.querySelector('#error-message-email') as HTMLLabelElement
+function checkEmail(): void {
+    const emailRegex: RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const validEmail: boolean = emailRegex.test(emailInput.value);
+    validEmail ? emailErrorMessege.style.display = 'none' : emailErrorMessege.style.display = 'block'
+}
 function checkInput(e: any): void {
     e.preventDefault();
-    firstNameInput.value === '' ?  firstNameErrorMessage.style.display = 'block' : firstNameErrorMessage.style.display = 'none';
+    checkEmail();
+    firstNameInput.value === '' ? firstNameErrorMessage.style.display = 'block' : firstNameErrorMessage.style.display = 'none';
     lastNameInput.value === '' ? lastNameErrorMessege.style.display = 'block' : lastNameErrorMessege.style.display = 'none';
 }
 submitButton.addEventListener('click', checkInput)
